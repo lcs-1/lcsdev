b4ee0f1c22d20a0bfe32e1b1f06d2503ea7801f0
lcs-1 <lcsx1945@gmail.com>
2024-11-13 20:50:20 +0530

    Add files via upload

--- to-do app/main.py
+++ to-do app/main.py
@@ -1,56 +1,63 @@
 while True:
     userinput = input("Please enter add or show or complete or edit or exit: ")
     userinput = userinput.lower().strip()
-    
-    if 'add' in userinput:
+
+    if userinput.startswith("add"):
         todo = userinput[4:]
-        
-        with open('todos.txt','r') as file:
+
+        with open("todos.txt", "r") as file:
             todos = file.readlines()
-            
-        todos.append(todo + '\n')
-        
-        with open('todos.txt','w') as file:
+
+        todos.append(todo + "\n")
+
+        with open("todos.txt", "w") as file:
             file.writelines(todos)
-    
-    elif 'show' in userinput:
-        with open('todos.txt','r') as file:
+
+    elif userinput.startswith("show"):
+        with open("todos.txt", "r") as file:
             todos = file.readlines()
-        
-        for index,task in enumerate(todos,start=1):
+
+        for index, task in enumerate(todos, start=1):
             print(f"Task {index}: {task.capitalize().strip()}")
-    
-    elif 'edit' in userinput:
-        number = userinput[5:]
-        number = int(number)-1
-        
-        with open('todos.txt','r') as file:
-            todos = file.readlines()
-            
-        edited_text = input("Please enter the corrected task: ")
-        todos[number] = edited_text + '\n'
-        
-        with open('todos.txt','w') as file:
-            file.writelines(todos)
-    
-    elif 'complete' in userinput:
-        number = userinput[9:]
-        number = int(number)-1
-        
-        with open('todos.txt','r') as file:
-            todos = file.readlines()
-        
-        print(f"{(todos[number]).strip('\n')} has been completed")
-        
-        todos.pop(number)
-        
-        with open('todos.txt','w') as file:
-            file.writelines(todos)
-    
-    elif 'exit' in userinput:
+
+    elif userinput.startswith("edit"):
+        try:
+            number = userinput[5:]
+            number = int(number) - 1
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            edited_text = input("Please enter the corrected task: ")
+            todos[number] = edited_text + "\n"
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+        except Exception:
+            print("Please enter the tasknumber with edit or enter correct tasknumber ")
+            continue
+
+    elif userinput.startswith("complete"):
+        try:
+            number = userinput[9:]
+            number = int(number) - 1
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            print(f"{(todos[number]).strip('\n')} has been completed")
+
+            todos.pop(number)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+        except Exception:
+            print("Please enter tasknumner next to complete or valid tasknumber")
+            continue
+    elif userinput.startswith("exit"):
         break
-    
+
     else:
         print("Please enter a valid input ")
-        
+
 print("Bye!")
